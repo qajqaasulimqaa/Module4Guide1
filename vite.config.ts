a12908dd69a9a1ee96777a696698838e28940054
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-11-03 13:30:04 +0000

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/guide1/',
+   base: '/Module4Guide1-/',
   plugins: [
     react(),
     tailwindcss(),
