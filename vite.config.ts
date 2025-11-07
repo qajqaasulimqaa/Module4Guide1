97530b56980a51299d87b20f4f944a81653aead6
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-11-07 13:28:04 +0000

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   base: '/Module4Guide1-/',
+   base: '/Module4Guide1/',
   plugins: [
     react(),
     tailwindcss(),
